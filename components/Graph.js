314cd5a2901182d496bc09ce2ad7e92a392babb9
Robert Duke <robduke123@gmail.com>
2025-02-22 16:35:19 +1300

    deploy changes

--- components/Graph.js
+++ components/Graph.js
@@ -50,7 +50,10 @@ export default function Graph() {
 		if (number + 1 === userList.length) {
 			return (
 				<div key={indx} className="w-px h-8 bg-white" style={{ height: "8px" }}>
-					<div className="relative" style={{ top: "18px", left: "-8px" }}>
+					<div
+						key={indx}
+						className="relative"
+						style={{ top: "18px", left: "-8px" }}>
 						{number + 1 < 10 ? "0" + (number + 1) : number + 1}
 					</div>
 				</div>
