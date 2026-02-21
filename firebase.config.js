e7688a5b2b79251af8797a1cce672ae8a57d36cd
Robert Duke <robduke123@gmail.com>
2026-02-21 16:04:19 +1300

    changed to firebase storage

--- firebase.config.js
+++ firebase.config.js
@@ -18,14 +18,14 @@ const access = `${process.env.ACCESS_TOKEN_SECRET}`;
 const refresh = `${process.env.REFRESH_TOKEN_SECRET}`;
 
 const firebaseConfig = {
-	apiKey: "AIzaSyA34RTnXqfS7EgXyQcNYbK-p3i5_L-dal8",
-	authDomain: "game-list-ae589.firebaseapp.com",
-	databaseURL: "https://game-list-ae589-default-rtdb.firebaseio.com",
-	projectId: "game-list-ae589",
-	storageBucket: "game-list-ae589.firebasestorage.app",
-	messagingSenderId: "951830935048",
-	appId: "1:951830935048:web:1cdf4647181d8210b803bd",
-	measurementId: "G-0KYTN1HT3E",
+	apiKey: `${process.env.FIREBASE_API_KEY}`,
+	authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+	databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
+	projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+	storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+	appId: `${process.env.FIREBASE_APP_ID}`,
+	measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
